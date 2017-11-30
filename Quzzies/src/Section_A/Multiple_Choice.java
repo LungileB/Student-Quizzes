11288ef3aec4eb1177c48d148ec3cf5f0b58f27d
Odwa <odwantontela@gmail.com>
2017-11-30 15:47:03 +0200

    Session

---
+++ Quzzies/src/Section_A/Multiple_Choice.java
@@ -0,0 +1,10 @@
+package Section_A;
+
+public class Multiple_Choice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("You Got 2Hours To Complete Your Quiz Session");
+	}
+
+}
